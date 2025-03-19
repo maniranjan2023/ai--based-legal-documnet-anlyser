08969d94a4b9dcc1278befe7857c08a05268f9ff
Maniranjan <137239336+maniranjan2023@users.noreply.github.com>
2025-03-19 21:21:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const iframe = document.getElementById("formFrame");
+
+    // Example: You can add event listeners if needed
+    iframe.onload = function () {
+        console.log("Iframe loaded successfully");
+    };
+});
+
+
